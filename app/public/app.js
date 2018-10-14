90532a09b3708e58d91efc657405945a145ac027
Harman <grewal.h98@gmail.com>
2018-10-13 23:51:44 -0700

    yelpapi works, but now event selection asynch probs

--- app/public/app.js
+++ app/public/app.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
-    const db = firebase.firestore();
-    const users = db.collection('users');
-    const groups = db.collection('groups');
-	const events = db.collection('events');	
-    var rapid = new RapidAPI("default-application_5bc03cd1e4b085e3f4089782", "9981cbba-80fd-41b4-b5bb-d76e8ee35535");
+	const db = firebase.firestore();
+	const settings = {timestampsInSnapshots: true};
+	db.settings(settings);
+    const users = db.collection("users");
+	const groups = db.collection("groups");
+	
+
 });
 
 
@@ -13,18 +15,82 @@ function googleLogin(){
     firebase.auth().signInWithPopup(provider)
 }
 
+function yelpAPI(){
+var rapid = new RapidAPI("default-application_5bc03cd1e4b085e3f4089782", "9981cbba-80fd-41b4-b5bb-d76e8ee35535");
 rapid.call('YelpAPI', 'searchEvent', { 
-	'startDate': '2018-10-12 00:00:00',
-	'coordinates': '49.263633, -123.246195',
+	'startDate': '2018-10-31 00:00:00',
+	'coordinates': '49.254336, -123.082367',
 	'accessToken': 'kI2t_FjvbnaP1MWJJ4HJipvZw0qWAWCUojI2djViwUgOvlnLGfDneZwZITiHqWiKgtmsusyrp-o_jSG1wAPwZbcxkMiXyD8BC_ISajmEvJuYUPBZMrl6IiULzQTAW3Yx',
 	'locale': 'en_CA',
 	'limit': '50',
-	'endDate': '2019-01-01 00:00:00',
+	'sortBy': 'desc',
+	'sortOn': 'time_start',
 	'location': 'Vancouver',
-	'radius': '39991'
+	'radius': '40000'
 
 }).on('success', function (payload) {
-	 /*YOUR CODE GOES HERE*/ 
+	var i;
+	eventsColl =firebase.firestore().collection("events");
+	for(i=0; i< payload.events.length; i++){
+		eventsColl.doc("event".concat(i.toString())).set(payload.events[i]);
+	}
 }).on('error', function (payload) {
-	 /*YOUR CODE GOES HERE*/ 
+	console.log('errrrrorrrr');
 });
+}
+
+function selectEvents(){
+	var category;
+	var count=0;
+	var max_cost=0;
+	var cateogry_map = {
+		"nightlife":0,
+		"charities":0,
+		"other":0,
+		"food-and-drink":0,
+		"sports-active-life":0,
+		"festivals-fair":0,
+		"visual-arts":0,
+		"performing-arts":0
+	};
+	var group1 = firebase.firestore().collection("groups").doc("group1");
+	
+	group1.get().then(function(doc) {
+	var data = doc.data();
+	var i;
+
+		
+	for(i=0; i<data.size; i++){
+		count++;
+		data.members[i].get().then(function(doc) {
+		var data1 = doc.data();
+		max_cost = max_cost +data1.cost_max;
+		cateogry_map[data1.category]++;
+	})
+}
+	category = geth(cateogry_map);
+	max_cost = max_cost/count;
+	console.log(category);
+	console.log(max_cost);
+	var setWithMerge = group1.set({
+		category: category,
+		cost_max: max_cost
+	}, { merge: true });
+	})
+	
+}
+
+function geth(o){
+    var vals = [];    
+    for(var i in o){
+       vals.push(o[i]);
+    }
+
+    var max = Math.max.apply(null, vals);
+
+     for(var i in o){
+        if(o[i] == max){
+            return i;
+        }
+    }
+}
