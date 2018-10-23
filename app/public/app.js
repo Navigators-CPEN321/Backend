118982605a0ed2d89a0153f32a4e9c48ce8273ac
Harman <grewal.h98@gmail.com>
2018-10-23 15:24:44 -0700

    demo stage

--- app/public/app.js
+++ app/public/app.js
@@ -104,6 +104,7 @@ function findGroupEvents(){
 	var group1 = firebase.firestore().collection("groups").doc("group1");
 	var events = firebase.firestore().collection("events");
 	var sel_events = group1.collection("sel_events");
+
 	group1.get().then(function(doc) {
 		var data = doc.data();
 		var i = 0;
@@ -134,3 +135,46 @@ function geth(o){
         }
     }
 }
+
+
+
+function deleteCollection(db, collectionPath, batchSize) {
+	var collectionRef = db.collection(collectionPath);
+	var query = collectionRef.orderBy('__name__').limit(batchSize);
+  
+	return new Promise((resolve, reject) => {
+	  deleteQueryBatch(db, query, batchSize, resolve, reject);
+	});
+  }
+  
+  function deleteQueryBatch(db, query, batchSize, resolve, reject) {
+	query.get()
+	  .then((snapshot) => {
+		// When there are no documents left, we are done
+		if (snapshot.size == 0) {
+		  return 0;
+		}
+  
+		// Delete documents in a batch
+		var batch = db.batch();
+		snapshot.docs.forEach((doc) => {
+		  batch.delete(doc.ref);
+		});
+  
+		return batch.commit().then(() => {
+		  return snapshot.size;
+		});
+	  }).then((numDeleted) => {
+		if (numDeleted === 0) {
+		  resolve();
+		  return;
+		}
+  
+		// Recurse on the next process tick, to avoid
+		// exploding the stack.
+		process.nextTick(() => {
+		  deleteQueryBatch(db, query, batchSize, resolve, reject);
+		});
+	  })
+	  .catch(reject);
+  }
